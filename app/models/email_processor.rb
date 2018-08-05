fe94a04711a7dbc2c1e38cf903562dd8085d41b2
usama ashraf <usama.ashraf.khan@gmail.com>
2018-08-06 04:23:57 +0500

    email process message created in db

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,7 +5,7 @@ class EmailProcessor
   end
   
   def process
-    vendor_id=VendorDispute.find_by(email: @email.from)
+    vendor_id=VendorDispute.find_by(email: @email.from[:email])
     VendorDisputeMessage.create!({ body: @email.body, email: @email.from, vendor_dispute_id: vendor_id.id })
   end
   
