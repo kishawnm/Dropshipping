10469535c8dd84b1c28fc57c89c72a9cfaace9b8
Usama Ashraf <Usama Ashraf>
2018-08-09 04:26:18 +0500

    quick fix

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,7 +7,7 @@ class EmailProcessor
   def process
     subject=@email.subject.delete! 'Re: '
     vendor_dispute=VendorDispute.find_by(subject: subject)
-    if @email.subject[:subject] ==vendor_dispute.subject
+    if subject ==vendor_dispute.subject
       VendorDisputeMessage.create!({ body: @email.body, email: @email.from[:email], vendor_dispute_id:vendor_dispute.id })
     end
   end
