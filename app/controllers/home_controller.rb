514876a04e7b0381f41454a645ff36bf110ad7d5
usamaashraf <usama.ashraf.khan@gmail.com>
2018-09-18 17:25:37 +0500

    fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,10 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+    if current_vendor.present?
+      redirect_to vendors_dashboard_index_path
+    else
+      redirect_to new_vendor_session_path
+    end
   end
 end
