2ff84767c6079440bd3411c82d5fa14890ee797a
usamaashraf <usama.ashraf.khan@gmail.com>
2018-09-20 18:02:06 +0500

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    shop = Shop.find_by(shopify_domain: params[:shop])
-    shop = ShopifyApp::SessionRepository.retrieve(shop.id)
-    puts shop
-    puts shop.id
+    # /shop = Shop.find_by(shopify_domain: params[:shop])
+    # shop = ShopifyApp::SessionRepository.retrieve(shop.id)
   end
 end
