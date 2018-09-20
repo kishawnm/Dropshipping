becfc9cc676341646d00918ea7eb4afab50297e6
usamaashraf <usama.ashraf.khan@gmail.com>
2018-09-20 19:55:17 +0500

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,29 +4,31 @@ class HomeController < ShopifyApp::AuthenticatedController
     @webhooks = ShopifyAPI::Webhook.find(:all)
     # /shop = Shop.find_by(shopify_domain: params[:shop])
     # shop = ShopifyApp::SessionRepository.retrieve(shop.id)
-    store = params[:shop]
-    user_name = store.split(".")[0]
-    vend = Vendor.where(store: store)
-    if vend.present?
-      vend = Vendor.where(store: store).last
-      sign_in :vendor, vend
-      if current_vendor.present?
-        redirect_to vendors_dashboard_index_path
-      else
-        redirect_to new_vendor_session_path
-      end
-    else
-      vendor = Vendor.new
-      vendor.email = "#{user_name}@example.com"
-      vendor.password = "#{user_name}"
-      vendor.password_confirmation = "#{user_name}"
-      vendor.store = store
-      if vendor.save
-        sign_in :vendor, vendor
+    if params[:shop].present?
+      store = params[:shop]
+      user_name = store.split(".")[0]
+      vend = Vendor.where(store: store)
+      if vend.present?
+        vend = Vendor.where(store: store).last
+        sign_in :vendor, vend
         if current_vendor.present?
           redirect_to vendors_dashboard_index_path
         else
           redirect_to new_vendor_session_path
+        end
+      else
+        vendor = Vendor.new
+        vendor.email = "#{user_name}@example.com"
+        vendor.password = "#{user_name}"
+        vendor.password_confirmation = "#{user_name}"
+        vendor.store = store
+        if vendor.save
+          sign_in :vendor, vendor
+          if current_vendor.present?
+            redirect_to vendors_dashboard_index_path
+          else
+            redirect_to new_vendor_session_path
+          end
         end
       end
     end
