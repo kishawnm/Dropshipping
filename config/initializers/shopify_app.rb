b20d816f2f6a61a1883a5766bc60152d4e8fec9d
usamaashraf <usama.ashraf.khan@gmail.com>
2018-09-21 19:42:48 +0500

    scopes

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name       = "Drop Shipping"
   config.api_key                = "c4cb3a84ba5ba3f28e147ca9d8c110e6"
   config.secret                 = "e86e8600ddd3d2326d3ca03818ae34a2"
-  config.scope                  = "read_orders, read_products"
+  config.scope                  = "read_orders, read_products, read_all_orders"
   config.embedded_app           = true
   config.after_authenticate_job = false
   config.session_repository     = Shop
