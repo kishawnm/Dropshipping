5ce36210d8fbdfb838f49d79ca1ca4a076261fab
Usama Ashraf <Usama Ashraf>
2018-09-22 02:09:48 +0500

    testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,8 +38,8 @@ class HomeController < ShopifyApp::AuthenticatedController
     require 'rubygems'
     require 'aftership'
     # send order id in params
-    # @orders = ShopifyAPI::Order.where(ids: params[:order_id])
-    @orders = ShopifyAPI::Order.find(:all, :params => {:status => 'any'})
+    # @orders = ShopifyAPI::Order.where(ids:)
+    @orders = ShopifyAPI::Orderfind(:all, :params => { ids:  params[:order_id] })
     puts "order details "*10
     puts @orders
     puts "order details "*10
