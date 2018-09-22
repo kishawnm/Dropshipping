12678223c4d5c6fc43ebb3f048b35a02d90cd84a
Usama Ashraf <Usama Ashraf>
2018-09-23 01:39:42 +0500

    testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -44,21 +44,24 @@ class HomeController < ShopifyApp::AuthenticatedController
     puts "*****"*10
     @orders=@orders.to_json
     obj    = JSON.parse(@orders)
-    sv2    = obj['fulfillments']
+    sv2    = obj['fulfillments'].first
     sv1    = obj['fulfillments'].first
     sv1=sv1['tracking_number']
     puts sv1
+    puts sv2
     puts "*****"*10
     # order_details = "#{current_vendor.store}/admin/orders/#{params[:order_id]}.json"
     # pluck tracking id from order_details object
     # tracking_no = order_details[:tracking_id]
     AfterShip.api_key = 'b00ab653-016a-48d7-9a4a-ae8072e6f41c'
-    tracking_status1  = AfterShip::V4::Tracking.detect({ :tracking_number => sv1 })
-    # tracking_status   = AfterShip::V4::Tracking.get(sv1,{})
+    # tracking_status1  = AfterShip::V4::Tracking.get.({ :tracking_number => sv1 })
+    tracking_status   = AfterShip::V4::Tracking.get('China Post',sv1)
     # puts tracking_status
     puts tracking_status1
     # get tracking status from tracking_status object
-    redirect_to vendors_dashboard_index_path(order: @orders)
+    redirect_to vendors_dashboard_index_path
   end
 
 end
+
+
