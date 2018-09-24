037e9325cc0dad4300d14d9885458076ccf541fe
Usama Ashraf <Usama Ashraf>
2018-09-25 00:17:58 +0500

    testing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -52,12 +52,13 @@ class HomeController < ShopifyApp::AuthenticatedController
     puts "*****"*10
     # order_details = "#{current_vendor.store}/admin/orders/#{params[:order_id]}.json"
     # pluck tracking id from order_details object
+    tracking_status1  = AfterShip::V4::Courier.detect({ :tracking_number => 'LY517551584CN' })
     # tracking_no = order_details[:tracking_id]
     AfterShip.api_key = 'b00ab653-016a-48d7-9a4a-ae8072e6f41c'
     # tracking_status1  = AfterShip::V4::Tracking.get.({ :tracking_number => sv1 })
-    tracking_status   = AfterShip::V4::Tracking.get('china-post',sv1)
+    # tracking_status   = AfterShip::V4::Tracking.get('china-post',sv1)
     # puts tracking_status
-    puts tracking_status
+    puts tracking_status1
     # get tracking status from tracking_status object
     redirect_to vendors_dashboard_index_path
   end
