45a2b5e3e1a548af27ea2efb00d38119610c2b8d
usamaashraf <usama.ashraf.khan@gmail.com>
2018-10-01 04:39:02 +0500

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -41,7 +41,7 @@ class HomeController < ShopifyApp::AuthenticatedController
         # orders = ShopifyAPI::Order.find(params[:order_id])
         # orders          = orders.to_json
         # obj             = JSON.parse(orders)
-        orders_list = ShopifyAPI::Order.find(:all)
+        orders_list = ShopifyAPI::Order.all
         orders_list_js         = orders_list.to_json
         obj_list             = JSON.parse(orders_list_js)
         obj_list.each do |obj|
