159b3388976527f2d084af7d83b3cee6057a402e
usamaashraf <usama.ashraf.khan@gmail.com>
2018-10-21 20:12:32 +0500

    fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 
 //= require jquery
 //= require jquery_ujs
-// = require bootstrap-sprockets
+//= require bootstrap-sprockets
 //= require custom
 //= require jquery.slimscroll
 //= require dashboard1
