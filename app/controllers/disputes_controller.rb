ce7c6d8b8c921c1428f47fa4af17d4639ff0857d
usamaashraf <usama.ashraf.khan@gmail.com>
2018-11-08 01:15:09 +0500

    fix

--- app/controllers/disputes_controller.rb
+++ app/controllers/disputes_controller.rb
@@ -4,7 +4,7 @@ class DisputesController < ApplicationController
     if params[:search].present?
       @issues = VendorDispute.search(params[:search],current_vendor.id)
     else
-      @issues = VendorDispute.joins(:vendor_dispute_messages).where(vendor_id: current_vendor.id).order(" vendor_dispute_messages.read ASC, vendor_dispute_messages.created_at DESC")
+      @issues = VendorDispute.joins(:vendor_dispute_messages).where(vendor_id: current_vendor.id).order(" vendor_dispute_messages.read ASC, vendor_dispute_messages.created_at DESC").uniq
     end
     @dispute = @issues.first
     if @dispute.present?
