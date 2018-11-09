54b9bbb018a5b8d0cae5607dce2493ca70368111
Usama Ashraf <Usama Ashraf>
2018-11-10 01:20:40 +0500

    form style changes

--- app/controllers/vendors_dashboard_controller.rb
+++ app/controllers/vendors_dashboard_controller.rb
@@ -156,7 +156,7 @@ class VendorsDashboardController < ApplicationController
   end
   
   def set_issues
-    @issues = VendorDispute.joins(:vendor_dispute_messages).where("vendor_id = ? AND vendor_disputes.created_at >= ?", current_vendor.id, Time.zone.now.beginning_of_day).order(" vendor_dispute_messages.read ASC, vendor_dispute_messages.created_at DESC")
+    @issues = VendorDispute.joins(:vendor_dispute_messages).where("vendor_id = ? AND vendor_disputes.created_at >= ?", current_vendor.id, Time.zone.now.beginning_of_day).order(" vendor_dispute_messages.read ASC, vendor_dispute_messages.created_at DESC").uniq
   end
   def set_presets
     @presets = ResponsePreset.all.where(vendor_id: current_vendor.id)
